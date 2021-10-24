eb49aad8b4e0dde87b56c726d4e5c1d75db7f361
Aiden-Jeon <ells2124@gmail.com>
2021-10-24 16:34:28 +0900

    add sentence calcuator

--- kowikiapi/main.py
+++ kowikiapi/main.py
@@ -1,3 +1,4 @@
+import os
 from typing import List
 from pathlib import Path
 
@@ -45,14 +46,45 @@ def extract(
     line_length: int = typer.Option(
         10, help="Skip sentence which is smaller than given length"
     ),
+    split_name: bool = typer.Option(False, is_flag=True, help="Split name with comma"),
 ):
     wiki_ko = wikipediaapi.Wikipedia("ko")
-    page = wiki_ko.page(name)
-    assert page.exists(), f"{name} is not proper article name."
-    lines = [page.text]
-    if kss:
-        lines = extarct_sentences(lines, line_length)
-    dump(name, lines, save_dir)
+    if split_name:
+        names = name.split(",")
+    else:
+        names = [name]
+    for name in names:
+        page = wiki_ko.page(name)
+        assert page.exists(), f"{name} is not proper article name."
+        lines = [page.text]
+        if kss:
+            lines = extarct_sentences(lines, line_length)
+        dump(name, lines, save_dir)
+
+
+@app.command()
+def lines(
+    path: str = typer.Option(..., help="Path to save extracted wiki"),
+    line_length: int = typer.Option(
+        10, help="Skip sentence which is smaller than given length"
+    ),
+):
+    import kss
+
+    path = Path(path)
+    total_length = 0
+    for file_path in path.glob("*.txt"):
+        sentences = []
+        with open(file_path, "r") as file_reader:
+            lines = file_reader.readlines()
+            sentence_list = map(kss.split_sentences, lines)
+            for sentence in sentence_list:
+                for line in sentence:
+                    if len(line) > line_length:
+                        sentences.append(line)
+        print(f"{file_path}:", len(sentences))
+        total_length += len(sentences)
+    print("Total sentence length:", total_length)
 
 
 def entry_point():
